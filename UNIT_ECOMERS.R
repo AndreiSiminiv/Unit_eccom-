a9d22ce29d4edb60d2d5a30a356bf56629494ab1
Andrei <123077618+AndreiSiminiv@users.noreply.github.com>
2023-03-21 23:32:57 +0300

    adding tables

--- UNIT_ECOMERS.R
+++ UNIT_ECOMERS.R
@@ -1,1 +1,29 @@
 #добавление таблиц 
+library(googlesheets4)
+library(tidyverse)
+library(dplyr)
+library(dtplyr)
+# аунтификация 
+gs4_auth_configure(path = 'D:/Обучение/GA/GOOLTB/googltabl.json')
+gs4_auth(email = 'asimonovnissan@gmail.com')
+httr::oauth_app(appname = "client_id",
+                key =  "338543304501-7bcighd0dgh57f6ta4cp13sjoevr66g2.apps.googleusercontent.com",
+                secret = "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")
+# Добавляем таблицу с продажами 
+sales <- as_sheets_id('https://docs.google.com/spreadsheets/d/1I5Ebye3_g0NmpV2qyGCqJiLDTNHUKPUxkG3_J_HzrrQ/edit#gid=0',
+                    '1I5Ebye3_g0NmpV2qyGCqJiLDTNHUKPUxkG3_J_HzrrQ/edit#gid=0')
+sales <-  range_read(sales, sheet = 'Данные по заказам и пользователям')
+# проверяем таблицу с продажами 
+head(sales)
+print(sales)
+# убираем значение NA  делаем 0
+sales$Revenue[is.na(sales$Revenue)] <- 0
+sales$`Product SKU`[is.na(sales$`Product SKU`)] <- 0
+sales$`Product Name`[is.na(sales$`Product Name`)] <- 0
+print(sales)
+#добавляем таблицу пролукты 
+tab12 <- as_sheets_id('https://docs.google.com/spreadsheets/d/1I5Ebye3_g0NmpV2qyGCqJiLDTNHUKPUxkG3_J_HzrrQ/edit#gid=0',
+                      '1I5Ebye3_g0NmpV2qyGCqJiLDTNHUKPUxkG3_J_HzrrQ/edit#gid=0')
+produkt <- range_read(tab12, 'Продукты')
+#добавляем таблицу расходы 
+consumption <- range_read(tab12,'Данные по расходам')
